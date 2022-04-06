bdc0dee595670643542c2aba37c30e32e720ef2d
Nitesh Kumar Verma <niteshverma7352@gmail.com>
2022-04-06 15:55:12 +0530

    update

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -2,18 +2,17 @@ var names=new Array();
 names[0]="Yaakov";
 names[1]="John";
 names[2]="Jen";
-names[3]="jason";
-names[4]="paul";
-names[5]="frank";
-names[6]="larry";
-names[7]="paula";
-names[8]="laura";
-names[9]="jim";
-
+names[3]="Jason";
+names[4]="Paul";
+names[5]="Frank";
+names[6]="Larry";
+names[7]="Paula";
+names[8]="Laura";
+names[9]="Lim";
 
 for (var i = 0; i < names.length; i++) {
 	if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
-        console.log("Goodbye "+ names[i])
+        console.log("Good Bye "+ names[i])
 	}
 	else{
 		console.log("Hello "+ names[i])
